70e8117c412df96f46f1c5881bf6ca7b149a378c
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-01-10 19:18:24 +0530

    Update main.tf

--- Terraform/security-group/main.tf
+++ Terraform/security-group/main.tf
@@ -108,22 +108,3 @@ resource "aws_security_group" "private-SG" {
     Name = "private-sg"
   }
 }
-
-/* Generate Key
-resource "tls_private_key" "rsa_4096" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Generate public Key 
-resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
-  public_key = tls_private_key.rsa_4096.public_key_openssh
-}
-
-# PEM Key download in system
-resource "local_file" "private_key" {
-  content = tls_private_key.rsa_4096.private_key_pem
-  filename = var.key_name
-}
-*/
