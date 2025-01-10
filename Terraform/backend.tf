16468d82f1feaa39fbda3c4011f7d8f38ac83a2c
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-01-10 19:27:52 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,1 +1,10 @@
+# terraform {
+#  backend "s3" {
+ #   bucket         = "your-bucket-name"            # Replace with your S3 bucket name
+  #  key            = "path/to/terraform.tfstate"  # File path in the bucket
+   # region         = "ap-south-1"                # AWS region (e.g., us-east-1)
+   # encrypt        = true                         # Enable encryption for the state file
+  #}
+#}
+
 
